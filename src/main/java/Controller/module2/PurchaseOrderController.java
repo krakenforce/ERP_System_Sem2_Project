4be51df322af91037f0d10f7ef3aca0a771f49bc
Sam Val <samvalentine2811@gmail.com>
2020-08-21 12:34:00 +0700

    fix conflict at cfg_tuan.fxml

--- src/main/java/Controller/module2/PurchaseOrderController.java
+++ src/main/java/Controller/module2/PurchaseOrderController.java
@@ -1,11 +1,37 @@
 package Controller.module2;
 
+import Services.Hibernate.DAO.CustomerDaoImpl;
+import Services.Hibernate.DAO.ProductDaoImpl;
+import Services.Hibernate.DAO.SalesManDaoImpl;
+import Services.Hibernate.entity.Customer;
+import Services.Hibernate.entity.Product;
+import Services.Hibernate.entity.Salesman;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.input.DragEvent;
 import javafx.scene.input.MouseEvent;
 
-public class PurchaseOrderController {
+import java.net.URL;
+import java.util.ResourceBundle;
 
+public class PurchaseOrderController  implements Initializable {
+
+    ProductDaoImpl pi = new ProductDaoImpl();
+    SalesManDaoImpl si = new SalesManDaoImpl();
+    CustomerDaoImpl ci = new CustomerDaoImpl();
+
+    public ChoiceBox<Product> productChoiceBox;
+    public ChoiceBox<Salesman> salesmanChoiceBox;
+    public ChoiceBox<Customer> customerChoiceBox;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        productChoiceBox.setItems((ObservableList<Product>) pi.getAllProducts());
+
+
+    }
     public void checkAmount(MouseEvent event) {
         System.out.println("sdflksdj");
     }
@@ -15,4 +41,5 @@ public class PurchaseOrderController {
 
     public void addToItems(ActionEvent event) {
     }
+
 }
