8e3d63805538716738dc8e95fc4ec070efd55416
Sam Val <samvalentine2811@gmail.com>
2020-08-21 10:20:55 +0700

    add MainTest to gitignore

--- src/main/java/MainTest.java
+++ src/main/java/MainTest.java
@@ -1,18 +1,6 @@
-import Services.Hibernate.DAO.LoginInfoDAO;
-import Services.Hibernate.DAO.SalesManDAO;
-import Services.Hibernate.entity.LoginInfo;
-import Services.Hibernate.entity.Salesman;
 
 public class MainTest {
     public static void main(String[] args) {
-        LoginInfo loginInfo = new LoginInfo();
 
-        loginInfo.setUsername("test");
-        loginInfo.setEmail("test@gmail.com");
-        loginInfo.setHashpw("adsfahsdfhuieruwrwebr");
-        loginInfo.setType(1);
-
-        LoginInfoDAO loginInfoDAO = new LoginInfoDAO();
-        loginInfoDAO.saveLoginInfo(loginInfo);
     }
 }
