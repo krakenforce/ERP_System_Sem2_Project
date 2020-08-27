691937ffff0d89d09f3c56a377508290d9798e60
Sam Val <samvalentine2811@gmail.com>
2020-08-27 17:53:56 +0700

    add some fields in trade discount columns

--- src/main/java/Controller/module2/TradeDiscountCols.java
+++ src/main/java/Controller/module2/TradeDiscountCols.java
@@ -1,4 +1,11 @@
 package Controller.module2;
 
+import java.sql.Date;
+
 public class TradeDiscountCols {
+    private Long id;
+    private int percentage;
+    private String name;
+    private Date start;
+    private Date end;
 }
